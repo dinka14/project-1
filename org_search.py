26f9750b8e4627dcad8507c1622a6096ec1ed39a
Denis Karpelevich <dkarpelevich@odin.com>
2018-02-22 10:03:54 +0300

    First test

--- org_search.py
+++ org_search.py
@@ -3,10 +3,12 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+
 class PythonOrgSearch(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Remote(command_executor='http://127.0.0.1:4444/wd/hub',desired_capabilities=DesiredCapabilities.CHROME)
+        self.driver = webdriver.Remote(command_executor='http://127.0.0.1:4444/wd/hub',
+                                       desired_capabilities=DesiredCapabilities.CHROME)
 
     def test_search_in_python_org(self):
         driver = self.driver
@@ -17,9 +19,9 @@ class PythonOrgSearch(unittest.TestCase):
         elem.send_keys(Keys.RETURN)
         assert "No results found." not in driver.page_source
 
-
     def tearDown(self):
         self.driver.close()
+
 
 if __name__ == "__main__":
     unittest.main()
