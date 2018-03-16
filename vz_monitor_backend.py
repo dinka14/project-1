e011f7ab6f5cd9081dcf26e2208609cbf68459f8
Denis Karpelevich <dkarpelevich@odin.com>
2018-03-16 13:14:03 +0300

    add set vnc pass

--- vz_monitor_backend.py
+++ vz_monitor_backend.py
@@ -25,9 +25,20 @@ class ExecuteCommand:
     def __init__(self, host, user, password):
         self.connect = ConnectBySSH(host, user, password)
 
-    def list_ve(self, command):
+    def check_ve_is_available(self, ve_name):
         connect = self.connect.connect()
-        stdin, stdout, stderr = connect.exec_command(command)
+        stdin, stdout, stderr = connect.exec_command('prlctl list -o name ' + ve_name)
+        data = stdout.read()
+        listq = data.split()
+        dictq = dict(zip(listq[::2], listq[1::2]))
+        if dictq == {}:
+            print 'There is no ve with this name on node'
+        else:
+            return True
+
+    def list_ve(self):
+        connect = self.connect.connect()
+        stdin, stdout, stderr = connect.exec_command("prlctl list -o name,status | sed '1d'")
         data = stdout.read()
         listq = data.split()
         dictq = dict(zip(listq[::2], listq[1::2]))
@@ -35,18 +46,24 @@ class ExecuteCommand:
 
     def stop_ve(self, ve_name):
         connect = self.connect.connect()
+        if not ExecuteCommand.check_ve_is_available(self, ve_name):
+            return
         stdin, stdout, stderr = connect.exec_command('prlctl stop ' + ve_name + ' && ' + 'prlctl list -a ' + ve_name)
         data = stdout.read()
         print(data)
 
     def start_ve(self, ve_name):
         connect = self.connect.connect()
+        if not ExecuteCommand.check_ve_is_available(self, ve_name):
+            return
         stdin, stdout, stderr = connect.exec_command('prlctl start ' + ve_name + ' && ' + 'prlctl list -a ' + ve_name)
         data = stdout.read()
         print(data)
 
     def set_vnc_password(self, ve_name, vnc_password):
         connect = self.connect.connect()
+        if not ExecuteCommand.check_ve_is_available(self, ve_name):
+            return
         stdin, stdout, stderr = connect.exec_command('prlctl stop ' + ve_name + ' && ' + 'prlctl set ' + ve_name +
                                                      ' --vnc-mode auto --vnc-passwd ' + vnc_password + ' && '
                                                      + 'prlctl start ' + ve_name)
@@ -69,7 +86,7 @@ class ExecuteCommand:
 
 
 ExecuteCommand('smoke.int.zone', 'root', '1q2w3e').set_vnc_password('ATest-dc84b23d647a.aqa.int.zone', '1q2w3e4r')
-
-# ExecuteCommand('smoke.int.zone', 'root', '1q2w3e').list_ve("prlctl list -o name,status | sed '1d'")
-# ExecuteCommand('smoke.int.zone', 'root', '1q2w3e').stop_ve('srv-6564eea3df74.aqa.int.zone')
-# ExecuteCommand('smoke.int.zone', 'root', '1q2w3e').start_ve('srv-6564eea3df74.aqa.int.zone')
+# ExecuteCommand('smoke.int.zone', 'root', '1q2w3e').check_ve_is_available('ATest-dc84b23d647a.aqa.int.zone')
+# ExecuteCommand('smoke.int.zone', 'root', '1q2w3e').list_ve()
+# ExecuteCommand('smoke.int.zone', 'root', '1q2w3e').stop_ve('!srv-6564eea3df74.aqa.int.zone')
+# ExecuteCommand('smoke.int.zone', 'root', '1q2w3e').start_ve('!srv-6564eea3df74.aqa.int.zone')
